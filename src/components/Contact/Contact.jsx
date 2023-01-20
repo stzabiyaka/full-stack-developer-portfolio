9242a631c189a638d55a87db8c42f02a1f5b6dd2
Stanislav Zabiyaka <stzabiyaka@gmail.com>
2023-01-20 09:55:11 +0200

    layout fix

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -2,23 +2,42 @@ import { CONSTANTS } from "../../constants/constants";
 import Section from "../Section/Section";
 import "./Contact.scss";
 
-const { contacts, icons } = CONSTANTS;
+const { contacts, title, name, familyName, icons } = CONSTANTS;
 
 const Contact = () => {
   return (
     <Section id="contact" title="Contact Details">
-      <ul className="contacts__list">
-        {contacts.map(({ name, title, link }, indx) => (
-          <li className="contact" key={name + indx}>
-            <a href={link} className="contact__link" title={name}>
-              <svg className="contact__icon">
-                <use href={`${icons}#icon-${[name]}`} />
-              </svg>
-              <p className="contact__title">{title}</p>
-            </a>
-          </li>
-        ))}
-      </ul>
+      <div className="contacts__card">
+        <div className="contacts__info">
+          <svg className="contacts__info-logo">
+            <use href={`${icons}#icon-logo`} />
+          </svg>
+          <p className="contacts__info-name">
+            {name} {familyName}
+          </p>
+          <p className="contacts__info-title">{title}</p>
+        </div>
+        <ul className="contacts__list">
+          {contacts.map(({ name, title, link }, indx) => (
+            <li className="contact" key={name + indx}>
+              <a
+                href={link}
+                className="contact__link"
+                title={name}
+                target={link.startsWith("http") ? "_blank" : "_self"}
+                rel="noreferrer noopener nofollow"
+              >
+                <div className="contact__icon">
+                  <svg className="contact__icon-image">
+                    <use href={`${icons}#icon-${[name]}`} />
+                  </svg>
+                </div>
+                <p className="contact__title">{title}</p>
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
     </Section>
   );
 };
